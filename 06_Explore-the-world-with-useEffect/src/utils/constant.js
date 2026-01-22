593238ecf5e715d718ba16dc37a9d5ee4d83e094
Prashant Singh <parshantsingh0717@gmail.com>
2026-01-22 16:03:03 +0530

    learned use of useEffect hook,created search-filter with swiggy api

---
+++ 06_Explore-the-world-with-useEffect/src/utils/constant.js
@@ -0,0 +1,3 @@
+export const imageUrl = `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_308,c_fill/`;
+export const APIURL =
+  "https://proxy.corsfix.com/?https://www.swiggy.com/dapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=22.56430&lng=88.36930&carousel=true&third_party_vendor=1";
